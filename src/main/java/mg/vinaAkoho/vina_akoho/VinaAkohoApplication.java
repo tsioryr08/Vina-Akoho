1d1abe12b154107904a0d1bd501561181b56fc89
tsioryr08 <rafalimananatsioryfandresena@gmail.com>
2026-06-13 20:50:10 +0300

    initialisation projet vinaAkoho et config: connexion PostgreSQL configurée

---
+++ src/main/java/mg/vinaAkoho/vina_akoho/VinaAkohoApplication.java
@@ -0,0 +1,13 @@
+package mg.vinaAkoho.vina_akoho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinaAkohoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinaAkohoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/vinaAkoho/vina_akoho/VinaAkohoApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.vinaAkoho.vina_akoho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinaAkohoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
