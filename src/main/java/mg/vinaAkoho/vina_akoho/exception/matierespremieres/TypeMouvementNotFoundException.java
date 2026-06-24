ea3f825789a47aa49dc8934cac9486337c93f4a1
Ranarison Antsa Rary <antsarary.ranarison@gmail.com>
2026-06-24 09:26:23 +0100

    Revert "Feature/rary matieres premieres"

--- src/main/java/mg/vinaAkoho/vina_akoho/exception/matierespremieres/TypeMouvementNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package mg.vinaAkoho.vina_akoho.exception.matierespremieres;
-
-public class TypeMouvementNotFoundException extends RuntimeException {
-
-    public TypeMouvementNotFoundException(String message) {
-        super(message);
-    }
-}
