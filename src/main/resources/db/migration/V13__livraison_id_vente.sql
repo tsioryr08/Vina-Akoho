995748aea79bfed1b2a7b3e8bc3a646f7571a419
ORGVSME <maudeck87@gmail.com>
2026-07-03 21:53:09 +0300

    fix : alter vente enlever

--- src/main/resources/db/migration/V13__livraison_id_vente.sql
+++ src/main/resources/db/migration/V13__livraison_id_vente.sql
@@ -4,12 +4,3 @@ ALTER TABLE livraison
 
 ALTER INDEX idx_livraison_commande
     RENAME TO idx_livraison_vente;
-
--- Ajouter id_client à la table vente
-ALTER TABLE vente
-    ADD COLUMN id_client INTEGER NOT NULL;
-
-ALTER TABLE vente
-    ADD CONSTRAINT fk_vente_client FOREIGN KEY (id_client) REFERENCES client(id) ON DELETE RESTRICT;
-
-CREATE INDEX idx_vente_client ON vente(id_client);
